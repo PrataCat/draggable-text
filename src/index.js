7a3bdd29a1353df5356fb4f50f3e65365a69d26c
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-12-22 16:57:30 +0200

    add replace logic

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const storageKey = "form-state";
 let data = {
   currentText: "",
 };
-
 let moveObjects = null;
-
+let coordsOfMovingObjX = null;
+let coordsOfMovingObjY = null;
 let isMoving = false;
+let currentObj = null;
+let marginObj = 4;
 
 //----------------call func---------------------------
 getSerializedData();
@@ -36,7 +38,6 @@ function onSubmit(e) {
   const arr = text.split("");
 
   createMarkup(arr);
-
   addListenerOnLetter();
   e.currentTarget.reset();
   localStorage.removeItem(storageKey);
@@ -60,6 +61,7 @@ function getSerializedData() {
     } else {
       const parsedData = JSON.parse(getData);
       userText.value = parsedData.currentText;
+      data.currentText = parsedData.currentText;
     }
   } catch (error) {
     console.error("get err", error.message);
@@ -69,18 +71,29 @@ function getSerializedData() {
 function createMarkup(data) {
   const markup = data
     .map(
-      (item) => `<span class="movable-element" draggable=true>${item}</span>`
+      (item) =>
+        `<span class="movable-element below" draggable=true>${item}</span>`
     )
     .join("");
 
   outputText.insertAdjacentHTML("afterbegin", markup);
 }
 
+function getCoords(obj) {
+  let coords = obj.getBoundingClientRect();
+  coordsOfMovingObjX = coords.left;
+  coordsOfMovingObjY = coords.top;
+}
+
+let currentDroppable = null;
+
 function addListenerOnLetter() {
   moveObjects = document.querySelectorAll(".movable-element");
 
   moveObjects.forEach(function (obj) {
     getCoords(obj);
+    obj.style.left = coordsOfMovingObjX + "px";
+    obj.style.top = coordsOfMovingObjY + "px";
   });
 
   moveObjects.forEach(function (obj) {
@@ -91,32 +104,78 @@ function addListenerOnLetter() {
     let shiftX = 0;
     let shiftY = 0;
 
+    function checkMovableBelow(e) {
+      obj.style.visibility = "hidden";
+      let objBelow = document.elementFromPoint(e.clientX, e.clientY);
+      obj.style.visibility = "visible";
+
+      if (!objBelow) return;
+
+      let movableBelow = objBelow.closest(".below");
+
+      if (currentDroppable != movableBelow) {
+        if (currentDroppable) {
+          leaveDroppable(currentDroppable);
+        }
+        currentDroppable = movableBelow;
+        if (currentDroppable) {
+          enterDroppable(currentDroppable);
+        }
+      }
+    }
+
     function moveAt(e) {
       isMoving = true;
       obj.style.left = e.pageX - shiftX + "px";
       obj.style.top = e.pageY - shiftY + "px";
+
+      checkMovableBelow(e);
+    }
+
+    function onMouseup(e) {
+      document.removeEventListener("mousemove", moveAt);
+      isMoving = false;
+      obj.style.cursor = "grab";
+      obj.style.zIndex = 0;
+      obj.classList.add("below");
+
+      if (!currentObj) return;
+      currentObj.style.left = coordsOfMovingObjX - marginObj + "px";
+      currentObj.style.top = coordsOfMovingObjY - marginObj + "px";
+      leaveDroppable(currentObj);
     }
 
     obj.addEventListener("mousedown", function (e) {
       if (isMoving) {
-        document.addEventListener("mouseup", function () {
-          document.removeEventListener("mousemove", moveAt);
-          isMoving = false;
-          obj.style.cursor = "grab";
-        });
+        document.addEventListener("mouseup", onMouseup);
       } else {
+        getCoords(obj);
         obj.style.cursor = "grabbing";
+        obj.classList.remove("below");
+        obj.style.zIndex = 10;
         shiftX = e.clientX - obj.getBoundingClientRect().left;
         shiftY = e.clientY - obj.getBoundingClientRect().top;
+        document.removeEventListener("mouseup", onMouseup);
         document.addEventListener("mousemove", moveAt);
       }
     });
   });
 }
 
-function getCoords(elem) {
-  let coords = elem.getBoundingClientRect();
+function enterDroppable(obj) {
+  obj.style.background = "yellow";
+  currentObj = obj;
+}
 
-  elem.style.left = coords.left + "px";
-  elem.style.top = coords.top + "px";
+function leaveDroppable(obj) {
+  obj.style.background = "";
+  currentObj = null;
+}
+
+function cancelDragstart() {
+  if (!moveObjects) return;
+
+  moveObjects.ondragstart = function () {
+    return false;
+  };
 }
